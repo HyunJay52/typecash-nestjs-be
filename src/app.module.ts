eb04e02efe0ba7333e79db74d1f01b841700b2df
Kim HyunJung <ctrlpjay@gmail.com>
2025-05-29 12:07:18 +0900

    feat: ad, point features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdModule } from './ad/ad.module';
+import { UserModule } from './user/user.module';
+import { PointModule } from './point/point.module';
 
 @Module({
-  imports: [],
+  imports: [AdModule, UserModule, PointModule],
   controllers: [AppController],
   providers: [AppService],
 })
